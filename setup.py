c6c257aaddaba6e955e4428013f8e46f40cb1171
Roberto Prevato <roberto.prevato@gmail.com>
2020-11-08 21:25:31 +0100

    v1.1.4 :octocat:

--- setup.py
+++ setup.py
@@ -8,15 +8,19 @@ def readme():
 
 setup(
     name="essentials",
-    version="1.1.3",
+    version="1.1.4",
     description="General purpose classes and functions, "
-                "reusable in any kind of Python application",
+    "reusable in any kind of Python application",
     long_description=readme(),
     long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
     url="https://github.com/RobertoPrevato/essentials",
@@ -24,11 +28,7 @@ setup(
     author_email="roberto.prevato@gmail.com",
     keywords="core utilities",
     license="MIT",
-    packages=[
-        "essentials",
-        "essentials.typesutils",
-        "essentials.decorators",
-    ],
-    install_requires=[],
+    packages=["essentials", "essentials.typesutils", "essentials.decorators"],
+    install_requires=["dataclasses==0.7;python_version<'3.7'"],
     include_package_data=True,
 )
